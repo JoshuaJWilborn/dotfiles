77c214c1b75be731ba2b756e779bfda248cf8173
Joshua Wilborn <joshua.j.wilborn@gmail.com>
2019-12-22 19:14:14 -0500

    remove backslash characters that dont work in all version

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#/usr/bin/bash
 if [ ! -d ./.git ]; then
   git clone git@github.com:JoshuaJWilborn/vim.git
   rsync -a ./vim/ .
@@ -6,10 +7,10 @@ if [ ! -d ./.git ]; then
 	git remote add origin git@github.com:JoshuaJWilborn/vim.git
 fi 
 
-pacman -S $(cat ./.packages)
-sudo pip install powerline-status
+sudo pacman --noconfirm -S $(cat ./.packages)
+sudo pip install powerline-status undervolt
 
-if [ ! -f $(which fzf) ]; then
+if [ ! $(command -v fzf) ]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
   rm -rf ~/.fzf
@@ -25,7 +26,7 @@ systemctl mask systemd-rfkill.service
 systemctl mask systemd-rfkill.socket
 
 # install linux brew
-if [ ! -f $(which brew) ]; then
+if [ ! $(command -v brew) ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
   test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
   test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
@@ -41,7 +42,7 @@ if [ ! -f /etc/systemd/system/undervolt.service ]; then
     [Service]
     Type=oneshot
     ExecStart=/usr/local/bin/undervolt -v --core -135 --cache -135 --gpu -135 --analogio -135 --uncore -135
-  END  
+END
 fi
 
 if [ ! -f /etc/systemd/system/undervolt-reset.service ]; then
@@ -51,5 +52,6 @@ if [ ! -f /etc/systemd/system/undervolt-reset.service ]; then
     [Service]
     Type=oneshot
     ExecStart=/usr/local/bin/undervolt -v --core -0 --cache -0 --gpu -0 --analogio -0 --uncore -0
-  END  
+END
 fi
+source ~/.bashrc
