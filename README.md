a4405acf46dd970b8faf15ffa3011f269a7d6cc8
Joshua Wilborn <joshua.j.wilborn@gmail.com>
2019-12-23 11:18:43 -0500

    update readme and install script

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Most of these are self explanatory.  You can easily install these dotfiles and y
 
 If you want to also install all the packages I use in an install, you can use the install.sh script which will install all packages (using pacman, so Arch distros) and install dotfiles etc.
 
+To use the install script call it like `sudo -u $USER install.sh`
+
+If you want to call it directly from terminal without pulling repo first: `sudo -u $USER bash $(curl https://raw.githubusercontent.com/joshuajwilborn/vim/master/install.sh)`
+
 If you only want to see a list of the packages required, view the .packages file.
 
 Sorry if things are a bit messy, I don't have much time to tidy these up.  I hope someone finds these useful!
--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #/usr/bin/bash
-if [ ! -d ./.git ]; then
-  git clone git@github.com:JoshuaJWilborn/vim.git
-  rsync -a ./vim/ .
-  rm -rf ./vim
+if [ ! -d ~/.git ]; then
+  git clone git@github.com:JoshuaJWilborn/vim.git ~/vim
+  rsync -a ~/vim/ ~/
+  rm -rf ~/vim
 	git remote remove origin
 	git remote add origin git@github.com:JoshuaJWilborn/vim.git
 fi 
 
-sudo pacman --noconfirm -S $(cat ./.packages)
+sudo pacman --needed --noconfirm -S $(cat ./.packages)
 sudo pip install powerline-status undervolt
 
 if [ ! $(command -v fzf) ]; then
@@ -19,11 +19,11 @@ fi
 nohup vim +PlugInstall +CocInstall +qall >/dev/null 2>&1 &
 
 sudo echo TLP_ENABLE=1 | sudo tee /usr/share/defaults/tlp
-systemctl enable tlp.service
-systemctl enable tlp-sleep.service 
-systemctl enable NetworkManager-dispatcher.service
-systemctl mask systemd-rfkill.service
-systemctl mask systemd-rfkill.socket
+sudo systemctl enable tlp.service
+sudo systemctl enable tlp-sleep.service 
+sudo systemctl enable NetworkManager-dispatcher.service
+sudo systemctl mask systemd-rfkill.service
+sudo systemctl mask systemd-rfkill.socket
 
 # install linux brew
 if [ ! $(command -v brew) ]; then
@@ -32,22 +32,22 @@ fi
 
 
 if [ ! -f /etc/systemd/system/undervolt.service ]; then
-  tee -a /etc/systemd/system/undervolt.service <<- END
+  sudo tee -a /etc/systemd/system/undervolt.service <<- END
     [Unit]
     Description=undervolt
     [Service]
     Type=oneshot
-    ExecStart=/usr/local/bin/undervolt -v --core -135 --cache -135 --gpu -135 --analogio -135 --uncore -135
+    ExecStart=`which undervolt` -v --core -135 --cache -135 --gpu -135 --analogio -135 --uncore -135
 END
 fi
 
 if [ ! -f /etc/systemd/system/undervolt-reset.service ]; then
-  tee -a /etc/systemd/system/undervolt-reset.service <<- END
+  sudo tee -a /etc/systemd/system/undervolt-reset.service <<- END
     [Unit]
     Description=undervolt
     [Service]
     Type=oneshot
-    ExecStart=/usr/local/bin/undervolt -v --core -0 --cache -0 --gpu -0 --analogio -0 --uncore -0
+    ExecStart=`which undervolt` -v --core -0 --cache -0 --gpu -0 --analogio -0 --uncore -0
 END
 fi
 source ~/.bashrc
